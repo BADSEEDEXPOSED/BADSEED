fb8b3a1f8ac402c33c05ff4fc672b01aafcb6b31
NINETENTWO <91178761+tradingbotswapmeet@users.noreply.github.com>
2025-12-03 22:45:28 -0800

    Fix storage.js: use X-Master-Key for all JSONBin operations

--- netlify/functions/lib/storage.js
+++ netlify/functions/lib/storage.js
@@ -29,7 +29,7 @@ class Storage {
                 method: options.method || 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    [process.env.NETLIFY_DEV ? 'X-Master-Key' : 'X-Access-Key']: JSONBIN_API_KEY,
+                    'X-Master-Key': JSONBIN_API_KEY,
                     ...options.headers
                 }
             };
